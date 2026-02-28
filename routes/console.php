f7fb9d4c67b4453d68eb6d2e02f55768605febdd
Matheusldsr <Matheusldsr01@gmail.com>
2026-02-28 17:18:22 -0300

    Ajuste pipe

--- routes/console.php
+++ routes/console.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-api.ts:41 
- POST http://localhost:8081/api/v1/orders/a9282681-393d-419d-9011-b39d78d3d226/pay 500 (Internal Server Error)
-request	@	api.ts:41
-post	@	api.ts:74
-pay	@	api.ts:171
-mutationFn	@	useOrders.ts:33
-await in execute		
-(anonymous)	@	Checkout.tsx:197
-await in (anonymous)		
-onClick	@	Checkout.tsx:208
 use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
