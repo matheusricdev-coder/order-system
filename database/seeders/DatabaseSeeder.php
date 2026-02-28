63a8037b2572b9370cc131d82bc46f6cca8f95d6
Matheusldsr <Matheusldsr01@gmail.com>
2026-02-28 16:08:30 -0300

    feat(seeders): add CustomerSeeder and include user seeders in DatabaseSeeder
    CustomerSeeder: customer@ordem.dev / Customer2026 (role=customer)
    AdminSeeder already existed: admin@ordem.dev / Admin2026 (role=admin)
    DatabaseSeeder now calls both after product/category/company seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             CompanySeeder::class,
             ProductSeeder::class,
+            AdminSeeder::class,
+            CustomerSeeder::class,
         ]);
     }
 }
